c3556858aacecf135bb80066ca93294def2fafd7
Jared Miles <jfmiles94@icloud.com>
2025-12-29 12:53:38 -0500

    Set up dict for request headers

--- tba-event-getter.py
+++ tba-event-getter.py
@@ -8,6 +8,13 @@ load_dotenv()
 jfmKey = os.getenv("jfmKey")
 authHead = "X-TBA-Auth-Key"
 
-baseApiURL = "https://www.thebluealliance.com/api/v3" # HOW!!!!!
+baseApiURL = "https://www.thebluealliance.com/api/v3"
+
+requestHeaders = {
+    authHead: jfmKey,
+    "Accept": "application/json"
+}
+
+requestParams = ""
 
 requests.request("GET", )
