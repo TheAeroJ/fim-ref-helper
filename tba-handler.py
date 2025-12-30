3c1efca14130af57275cb4e22b5c5997a90f1c9e
Jared Miles <jfmiles94@icloud.com>
2025-12-29 23:35:08 -0500

    Set up script to use command line arguments

--- tba-handler.py
+++ tba-handler.py
@@ -4,20 +4,28 @@ import json
 import os
 import requests
 
-load_dotenv()
-
-jfmKey = os.getenv("jfmKey")
-authHead = "X-TBA-Auth-Key"
-
-baseApiURL = "https://www.thebluealliance.com/api/v3"
-
-requestHeaders = {
-    authHead: jfmKey,
-    "Accept": "application/json"
-}
+# Parse arguments
 
 def main():
-    return
+    load_dotenv()
+
+    jfmKey = os.getenv("jfmKey")
+    authHead = "X-TBA-Auth-Key"
+
+    baseApiURL = "https://www.thebluealliance.com/api/v3"
+
+    requestHeaders = {
+        authHead: jfmKey,
+        "Accept": "application/json"
+    }
+
+def scriptConfig():
+    # Add arguments as needed
+    return parser.parse_args()
+    
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="TBA Event Getter Script")
+    parser.add_argument('--suffix', type=str, help='API suffix string to be appended to base URL', required=True, default='/status')
+    args = parser.parse_args()
+    main(args.suffix)
